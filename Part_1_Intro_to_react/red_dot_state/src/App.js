e26e5532bc4bbb8bcda65180f6488e0474474f09
Suprame4 <adriancortesgonz@gmail.com>
2022-12-15 18:32:31 -0800

    Create the red moving dot app

---
+++ Part_1_Intro_to_react/red_dot_state/src/App.js
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+export default function MovingDot() {
+  //create a state object 
+  const [position, setPosition] = useState({
+    x: 0,
+    y: 0
+  });
+  return (
+    <div
+      //create a event handler function to change the state
+      onPointerMove={e => {
+        //use the state setting function
+        setPosition({
+          x: e.clientX,
+          y: e.clientY
+        });
+      }}
+      style={{
+        position: 'relative',
+        width: '100vw',
+        height: '100vh',
+      }}>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'red',
+        borderRadius: '50%',
+        transform: `translate(${position.x}px, ${position.y}px)`,
+        left: -10,
+        top: -10,
+        width: 20,
+        height: 20,
+      }} />
+    </div>
+  );
+}
