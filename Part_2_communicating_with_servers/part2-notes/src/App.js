4b988b359a1a38208426324dabe26fb7b3f92613
Suprame4 <adriancortesgonz@gmail.com>
2022-12-23 17:55:27 -0800

    Complete altering data in the server

--- Part_2_communicating_with_servers/part2-notes/src/App.js
+++ Part_2_communicating_with_servers/part2-notes/src/App.js
@@ -1,23 +1,21 @@
-import { useState } from 'react'
-import Note from './components/Note'
+import { useState, useEffect } from 'react'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+import Note from './components/Note'
+import noteService from './services/notes'
+
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('') 
   const [showAll, setShowAll] = useState(true)
 
-  const addNote = (event) => {
-    event.preventDefault()
-    const noteObject = {
-      content: newNote,
-      date: new Date().toISOString(),
-      important: Math.random() > 0.5,
-      id: notes.length + 1,
-    }
-
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
-  }
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
+      })
+  }, [])
+  //console.log('render', notes.length, 'notes')
 
   const handleNoteChange = (event) => {
     console.log(event.target.value)
@@ -28,12 +26,53 @@ const App = (props) => {
     ? notes
     : notes.filter(note => note.important)
 
+  const toggleImportanceOf = id => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+  
+    noteService
+        .update(id, changedNote)
+        .then(response => {
+          setNotes(notes.map(note => note.id !== id ? note : response.data))
+        })
+        .catch(error => {
+          alert(
+            `the note '${note.content}' was already deleted from server`
+          )
+          setNotes(notes.filter(n => n.id !== id))
+        })
+    }
+  
+  const addNote = (event) => {
+      event.preventDefault()
+      const noteObject = {
+        content: newNote,
+        date: new Date().toISOString(),
+        important: Math.random() > 0.5
+      }
+  
+    noteService
+        .create(noteObject)
+        .then(response => {
+          setNotes(notes.concat(response.data))
+          setNewNote('')
+        })
+    }
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
       <ul>
         {notesToShow.map(note => 
-          <Note key={note.id} note={note} />
+          <Note key={note.id} 
+            note={note} 
+            toggleImportance={() => toggleImportanceOf(note.id) }
+          />
         )}
       </ul>
       <form onSubmit={addNote}>
