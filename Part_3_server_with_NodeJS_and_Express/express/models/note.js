ba96d22c8a091ac9188590c8b7ad89eeb6282012
Suprame4 <adriancortesgonz@gmail.com>
2023-03-25 18:15:56 -0700

    change the enviornment variable in note.js line 5 from 'MONGODB_URI' to 'MONGODB_URL'

--- Part_3_server_with_NodeJS_and_Express/express/models/note.js
+++ Part_3_server_with_NodeJS_and_Express/express/models/note.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', false)
 
-const url = process.env.MONGODB_URI
+const url = process.env.MONGODB_URL
 
 console.log('connecting to', url)
 
