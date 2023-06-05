e5a1d97948360fb2174590e08cb0a2489d1342d6
Adrian G <adriancortesgonz@gmail.com>
2023-06-04 20:41:04 -0700

    remove phonebook dir

--- Part_4_Testing_Express_servers/phonebook/src/components/Note.js
+++
@@ -1,9 +0,0 @@
-const Note = ({ note, remove }) => {
-    return (
-      <li>{note.name} {note.number}
-        <button onClick={remove}>delete</button>
-      </li>
-    )
-  }
-  
-  export default Note
