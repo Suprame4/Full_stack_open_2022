b9aa56580e7a2e706bbbb74ac2a55f98e8bcc2df
Adrian G <adriancortesgonz@gmail.com>
2023-06-04 21:12:27 -0700

    add notes backend dir

---
+++ Part_4_Testing_Express_servers/express/utils/logger.js
@@ -0,0 +1,13 @@
+//separate all printing to the console to its own module utils/logger.js
+
+const info = (...params) => {
+    console.log(...params)
+  }
+  
+  const error = (...params) => {
+    console.error(...params)
+  }
+  
+  module.exports = {
+    info, error
+  }
