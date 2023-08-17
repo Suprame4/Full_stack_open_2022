de5d2780742222ab1ded60b461e233168fe41aa5
Adrian G <adriancortesgonz@gmail.com>
2023-08-16 17:59:50 -0700

    complete section b (testing the backend) but tests are failing

--- Part_4_Testing_Express_servers/express/controllers/notes.js
+++ Part_4_Testing_Express_servers/express/controllers/notes.js
@@ -1,25 +1,27 @@
 const notesRouter = require('express').Router() //create a new router object 
 const Note = require('../models/note')
 
-notesRouter.get('/', (request, response) => {
+notesRouter.get('/', async (request, response) => {
+  const notes = await Note.find({})
+  response.json(notes)
+  /*
   Note.find({}).then(notes => {
     response.json(notes)
-  })
+  })*/
 })
 
-notesRouter.get('/:id', (request, response, next) => {
-  Note.findById(request.params.id)
-    .then(note => {
-      if (note) {
-        response.json(note)
-      } else {
-        response.status(404).end()
-      }
-    }) 
-    .catch(error => next(error))
+notesRouter.get('/:id', async (request, response) => {
+
+  const note = await Note.findById(request.params.id)
+  if(note){
+    response.json(note);
+  }
+  else {
+    response.status(404).end()
+  }
 })
 
-notesRouter.post('/', (request, response, next) => {
+notesRouter.post('/', async (request, response) => {
   const body = request.body
 
   const note = new Note({
@@ -27,19 +29,24 @@ notesRouter.post('/', (request, response, next) => {
     important: body.important || false,
   })
 
-  note.save()
-    .then(savedNote => {
-      response.json(savedNote)
-    })
-    .catch(error => next(error))
+  
+  const savedNote = await note.save()
+  response.status(201).json(savedNote)
 })
 
-notesRouter.delete('/:id', (request, response, next) => {
-  Note.findByIdAndRemove(request.params.id)
-    .then(() => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
+notesRouter.delete('/:id', async (request, response) => {
+  
+  await Note.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+
+  //the express-async-errors allows you to eliminate the try-catch blocks
+  /*try{
+    await Note.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+  }
+  catch(exeption){
+    next(exception)
+  }*/
 })
 
 notesRouter.put('/:id', (request, response, next) => {
