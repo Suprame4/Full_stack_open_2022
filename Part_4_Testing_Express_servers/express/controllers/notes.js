c182abae69e1fcc30b46ee0ebfdff80abdb53e3e
Adrian G <adriancortesgonz@gmail.com>
2023-08-19 18:52:43 -0700

    complete section c (user administration) but users GET request does not return notes associated with user

--- Part_4_Testing_Express_servers/express/controllers/notes.js
+++ Part_4_Testing_Express_servers/express/controllers/notes.js
@@ -1,13 +1,12 @@
 const notesRouter = require('express').Router() //create a new router object 
 const Note = require('../models/note')
+const User = require('../models/user')
 
 notesRouter.get('/', async (request, response) => {
-  const notes = await Note.find({})
+  const notes = await Note
+  .find({}).populate('user', { username: 1, name: 1 })
+
   response.json(notes)
-  /*
-  Note.find({}).then(notes => {
-    response.json(notes)
-  })*/
 })
 
 notesRouter.get('/:id', async (request, response) => {
@@ -24,13 +23,17 @@ notesRouter.get('/:id', async (request, response) => {
 notesRouter.post('/', async (request, response) => {
   const body = request.body
 
+  const user = await User.findById(body.userId)
+
   const note = new Note({
     content: body.content,
-    important: body.important || false,
+    important: body.important === undefined ? false : body.important,
+    user: user.id
   })
 
   
   const savedNote = await note.save()
+  user.notes = user.notes.concat(savedNote._id)
   response.status(201).json(savedNote)
 })
 
--- Part_4_Testing_Express_servers/express/tests/note_api.test.js
+++ Part_4_Testing_Express_servers/express/tests/note_api.test.js
@@ -7,20 +7,77 @@ const api = supertest(app)
 
 const Note = require('../models/note')
 
-beforeEach(async () => {
-    await Note.deleteMany({})
-    await Note.insertMany(helper.initialNotes)
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
 
-    /*
-    const noteObjects = helper.initialNotes
-        .map(note => new Note(note))
-    //Promise.all executes the promises it receives in parallel
-    const promiseArray = noteObjects.map(note => note.save())
-    await Promise.all(promiseArray)
-    */
-}, 100000)
+describe('when there is initially one user in db', () => {
+    beforeEach(async () => {
+        await User.deleteMany({})
+
+        const passwordHash = await bcrypt.hash('sekret', 10)
+        const user = new User({ username: 'root', passwordHash })
+
+        await user.save()
+    })
+
+    test('creation fails with proper statuscode and message if username already taken', async () => {
+        const usersAtStart = await helper.usersInDb()
+
+        const newUser = {
+            username: 'root',
+            name: 'Superuser',
+            password: 'salainen',
+        }
+
+        const result = await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(400)
+            .expect('Content-Type', /application\/json/)
+
+        expect(result.body.error).toContain('expected `username` to be unique')
+
+        const usersAtEnd = await helper.usersInDb()
+        expect(usersAtEnd).toEqual(usersAtStart)
+    })
+
+    test('creation succeeds with a fresh username', async () => {
+        const usersAtStart = await helper.usersInDb()
+
+        const newUser = {
+            username: 'mluukkai',
+            name: 'Matti Luukk',
+            password: 'salainen',
+        }
+
+        await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+
+        const usersAtEnd = await helper.usersInDb()
+        expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+        const usernames = usersAtEnd.map(u => u.username)
+        expect(usernames).toContain(newUser.username)
+    })
+})
 
 describe("when there is initially some notes saved", () => {
+    
+    beforeEach(async () => {
+        await Note.deleteMany({})
+        await Note.insertMany(helper.initialNotes)
+
+        /*
+        const noteObjects = helper.initialNotes
+            .map(note => new Note(note))
+        //Promise.all executes the promises it receives in parallel
+        const promiseArray = noteObjects.map(note => note.save())
+        await Promise.all(promiseArray)
+        */
+    }, 100000)
     test('notes are returned as json', async () => {
     console.log('entered test');
     await api
