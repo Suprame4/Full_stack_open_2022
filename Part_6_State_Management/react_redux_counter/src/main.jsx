11e66bdd2fe99cfe8c8b9f9fe68e99b548f9cb4b
Adrian G <adriancortesgonz@gmail.com>
2023-10-25 21:39:32 -0700

    Add an input element to add notes utilizing the redux store

--- Part_6_State_Management/react_redux_counter/src/main.jsx
+++ Part_6_State_Management/react_redux_counter/src/main.jsx
@@ -2,45 +2,68 @@ import * as React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import { createStore } from 'redux';
-
-// create the reducer function
-const counterReducer = (state = 0, action) => {
-  switch (action.type){
-    case 'INCREMENT': 
-      return state + 1;
-    case 'DECREMENT':
-      return state - 1;
-    case 'ZERO':
-      return 0;
-    default:
-      return state;  
-  }
-}
+import { noteReducer } from './reducers/noteReducer';
 
 // create the store 
-const store = createStore(counterReducer);
+const store = createStore(noteReducer);
 
+// example dispatch 
+store.dispatch({
+  type: 'NEW_NOTE',
+  payload: {
+    content: 'the app state is in redux store',
+    important: true,
+    id: 1
+  }
+})
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  payload: {
+    content: 'state changes are made with actions',
+    important: false,
+    id: 2
+  }
+})
+
+const generateId = () => Number((Math.random() * 1000000).toFixed(0))
 const App = () => {
+  const addNote = (event) => {
+    event.preventDefault()
+    const content = event.target.note.value;
+    event.target.note.value = '';
+    store.dispatch({
+      type: 'NEW_NOTE',
+      payload: {
+        content,
+        important: false,
+        id: generateId()
+      }
+    })
+  }
+
+  const toggleImportance = (id) => {
+    store.dispatch({
+      type: 'TOGGLE_IMPORTANCE',
+      payload: { id }
+    })
+  }
   return (
     <div>
-      <div>
-        {store.getState()}
-      </div>
-      <button
-        onClick={e => store.dispatch({ type: 'INCREMENT' })}
-      >
-        plus
-      </button>
-      <button
-        onClick={e => store.dispatch({ type: 'ZERO' })}
-      >
-        zero
-      </button>
-      <button
-        onClick={e => store.dispatch({ type: 'DECREMENT' })}
-      >
-        minus
-      </button>
+      <form onSubmit={addNote}>
+        <input name='note' />
+        <button type='submit'>add</button>
+      </form>
+      <ul>
+        {store.getState().map(note => 
+          <li 
+            key={note.id}
+            onClick={() => toggleImportance(note.id)}
+          >
+            {note.content} <strong>{note.important ? 'important' : 'not-important'}</strong>
+          </li>
+        )}
+      </ul>
     </div>
   )
 }
@@ -53,4 +76,4 @@ const renderApp = () => {
 }
 
 renderApp();
-store.subscribe(renderApp);
+store.subscribe(renderApp); 
