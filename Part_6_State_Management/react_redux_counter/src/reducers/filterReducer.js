425546c451f0545aa75f15b54896cdecbf0d508a
Adrian G <adriancortesgonz@gmail.com>
2023-10-28 20:05:22 -0700

    refactor the filter reducer

--- Part_6_State_Management/react_redux_counter/src/reducers/filterReducer.js
+++ Part_6_State_Management/react_redux_counter/src/reducers/filterReducer.js
@@ -1,18 +1,31 @@
+import * as React from 'react';
+import { createSlice } from '@reduxjs/toolkit'
 
-const filterReducer = (state = 'ALL', action) => {
-    switch (action.type){
-        case 'SET_FILTER':
+// const filterReducer = (state = 'ALL', action) => {
+//     switch (action.type){
+//         case 'SET_FILTER':
+//             return action.payload
+//         default:
+//             return state
+//     }
+// }
+
+// export const filterChange = filter => {
+//     return {
+//         type: 'SET_FILTER',
+//         payload: filter,
+//     }
+// }
+const initialState = 'ALL';
+const filterSlice = createSlice({
+    name: 'filters',
+    initialState,
+    reducers: {
+        filterChange( state, action ){
             return action.payload
-        default:
-            return state
+        }
     }
-}
+})
 
-export const filterChange = filter => {
-    return {
-        type: 'SET_FILTER',
-        payload: filter,
-    }
-}
-
-export default filterReducer;
+export const { filterChange } = filterSlice.actions;
+export default filterSlice.reducer;
