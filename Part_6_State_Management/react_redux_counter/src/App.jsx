96e73edd3807d25e49b94df2d7092d66a39c96dd
Adrian G <adriancortesgonz@gmail.com>
2023-12-01 17:43:38 -0800

    update

--- Part_6_State_Management/react_redux_counter/src/App.jsx
+++ Part_6_State_Management/react_redux_counter/src/App.jsx
@@ -1,16 +1,41 @@
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { getNotes, createNote, updateNote } from './requests'
+
 const App = () => {
+  const queryClient = useQueryClient()
+
+  const newNoteMutation = useMutation( createNote, {
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['notes'] })
+    }
+  })
+
   const addNote = async (event) => {
     event.preventDefault()
     const content = event.target.note.value
     event.target.note.value = ''
-    console.log(content)
+    newNoteMutation.mutate({ content, important: true })
   }
 
+  const updateNoteMutation = useMutation(updateNote, {
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['notes'] })
+    },
+  })
+  
   const toggleImportance = (note) => {
-    console.log('toggle importance of', note.id)
+    updateNoteMutation.mutate( {...note, important: !note.important } )
+  }
+  const result = useQuery({
+    queryKey: ['notes'],
+    queryFn: getNotes
+  })
+  console.log(JSON.parse(JSON.stringify(result)))
+  if( result.isLoading ){
+    return <div>Loading data...</div>
   }
 
-  const notes = []
+  const notes = result.data; 
 
   return(
     <div>
--- Part_6_State_Management/react_redux_counter/src/main.jsx
+++ Part_6_State_Management/react_redux_counter/src/main.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider} from '@tanstack/react-query'
 
-import App from './App'
+import App from './App';
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
 )
