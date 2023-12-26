959a3fcdb0152834c75c144c4361f65e804a1c6c
Adrian G <adriancortesgonz@gmail.com>
2023-12-25 19:07:05 -0800

    initial webpack commit

---
+++ Part_7_Router_and_Webpack/webpack-app/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Webpack App</h1>
+      <div>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p>
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
