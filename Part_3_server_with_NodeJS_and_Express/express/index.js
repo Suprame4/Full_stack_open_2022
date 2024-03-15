290fce9a8292e74355fe1593075493af8a2643cf
Adrian G <adriancortesgonz@gmail.com>
2024-03-14 21:18:59 -0700

    Revert express to part 3 section c - Saving Data to MongoDB

--- Part_3_server_with_NodeJS_and_Express/express/index.js
+++ Part_3_server_with_NodeJS_and_Express/express/index.js
@@ -1,8 +1,121 @@
-const app = require('./app') // the actual Express app
-const config = require('./utils/config')
-const logger = require('./utils/logger')
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const Note = require('./models/note')
+
+//update for cross origin resource sharing 
+const cors = require('cors')
 
 
-app.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`)
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path: ', request.path)
+  console.log('Body: ', request.body)
+  console.log('---')
+  next()
+  }
+
+//create errorHandler 
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message)
+
+  if (error.name === 'CastError'){
+    return response.status(400).send({ error: 'malformatted id'})
+  } else if (error.name === 'ValidationError'){
+    return response.status(400).json({ error: error.message})
+  }
+
+  next(error)
+}
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({error: 'unknown endpoint'})
+}
+
+app.use(express.json())
+app.use(requestLogger)
+app.use(cors())
+app.use(express.static('build'))
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+
+//view the notes 
+app.get('/api/notes', (resquest, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes) 
+  })
+})
+
+
+
+app.get('/api/notes/:id', (request, response) => {
+   Note.findById(request.params.id)
+   .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+   })
+   .catch(error => next(error))
+})
+
+//deleting resources
+app.delete('/api/notes/:id', (request, response) => {
+    Note.findByIdAndRemove(request.params.id)
+      .then(result => {
+        response.status(204).end()
+      })
+      .catch( error => next(error))
+})
+
+const generateId = () => {
+    const maxId = notes.length > 0
+        ? Math.max(...notes.map(n => n.id))
+        : 0
+    return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+    const body = request.body 
+    
+    console.log(request.body.content)
+    if(!body.content === undefined){
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+    const note =  new Note({
+        content: body.content,
+        important: body.important || false,
+    })
+
+    note.save().then(savedNote => {
+      response.json(savedNote)
+      //close connection
+      //mongoose.connection.close()
+    })
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const {content, important} = request.body
+
+  Note.findByIdAndUpdate(
+    request.params.id, 
+    {content, important}, 
+    {new: true, runValidators: true, context: 'query'})
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
